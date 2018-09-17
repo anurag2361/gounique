b88834693302db53987935e6cf26f1a530cc659b
anurag <sharmaanurag.236@gmail.com>
2018-09-17 09:32:59 +0530

    small correction

--- gounique.go
+++ gounique.go
@@ -3,8 +3,8 @@ package unique
 //Int returns unique int values
 func Int(slice []int) map[int]struct{} {
 	uMap := make(map[int]struct{})
-	for key := range slice {
-		uMap[key] = struct{}{}
+	for _, val := range slice {
+		uMap[val] = struct{}{}
 	}
 	return uMap
 }
