920818175bfe38f3adb402322e583f47dbfc7ebf
anurag <sharmaanurag.236@gmail.com>
2018-09-17 09:56:18 +0530

    added string method

--- gounique.go
+++ gounique.go
@@ -1,8 +1,17 @@
 package unique
 
-//Int returns unique int values
+//Int returns unique int values in am map paired with an empty struct than can be avoided
 func Int(slice []int) map[int]struct{} {
 	uMap := make(map[int]struct{})
+	for _, val := range slice {
+		uMap[val] = struct{}{}
+	}
+	return uMap
+}
+
+//Strings returns unique string values in a map paired with an empty struct than can be avoided
+func Strings(slice []string) map[string]struct{} {
+	uMap := make(map[string]struct{})
 	for _, val := range slice {
 		uMap[val] = struct{}{}
 	}
