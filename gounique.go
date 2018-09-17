596866cdeda77d7cb9a6f313f1229e285e692db0
anurag <sharmaanurag.236@gmail.com>
2018-09-17 15:22:54 +0530

    functions now return slices instead of maps

--- gounique.go
+++ gounique.go
@@ -1,19 +1,31 @@
 package unique
 
-//Int returns unique int values in a map paired with an empty object
-func Int(slice []int) map[int]struct{} {
+import "sort"
+
+//Int returns unique int values in a slice
+func Int(slice []int) []int {
 	uMap := make(map[int]struct{})
+	result := []int{}
 	for _, val := range slice {
 		uMap[val] = struct{}{}
 	}
-	return uMap
+	for key := range uMap {
+		result = append(result, key)
+	}
+	sort.Ints(result)
+	return result
 }
 
-//Strings returns unique string values in a map paired with an empty object
-func Strings(slice []string) map[string]struct{} {
+//Strings returns unique string values in a slice
+func Strings(slice []string) []string {
 	uMap := make(map[string]struct{})
+	result := []string{}
 	for _, val := range slice {
 		uMap[val] = struct{}{}
 	}
-	return uMap
+	for key := range uMap {
+		result = append(result, key)
+	}
+	sort.Strings(result)
+	return result
 }
